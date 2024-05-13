99cdceb325092992c7c00314e8687cd20193823a
Reaganosh <gunawanr@spu.edu>
2024-05-13 01:16:57 -0700

    Figured out command line stuff

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <String>
 
-int main()
-{
-    std::cout << "Hello world!" << std::endl;
+int main(int argc, char* argv[]) {
+    std::string action = argv[1];
+    std::string fileName = argv[2];
+
+    if(argc < 4 && action == "encrypt") {
+        std::cout << "File has been encrypted!" << std::endl;
+    }
+    else if(argc < 4 && action == "decrypt") {
+        std::cout << "File has been decrypted!" << std::endl;
+    }
+    else {
+        std::cout << "Invalid command! Please enter a valid one." << std::endl;
+    }
+
     return 0;
 }
