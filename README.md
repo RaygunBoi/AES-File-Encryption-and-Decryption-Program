04a1a9a2badcca4535f8e5167ba68e9400113db1
Reaganosh <gunawanr@spu.edu>
2024-05-15 01:47:18 -0700

    done for now

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# AES-File-Encryption-and-Decryption-Program
+## Introduction	
+> This program utilizes two different algorithms to encrypt and decrypt a simple text file: the Vigenere Cipher and the Columnar Transitional Cipher. Both algorithms were programmed entirely in C++, and an example file called `example.txt` is provided to test them both.
+
+## How To Use
+- Make sure you have the g++ (or gcc) compiler installed.
+- Clone the repository, open command prompt, and navigate to the directory: `cd Vigenere-and-Columnar-Transposition-Encryption-and-Decryption`
+- To run the program, enter the following command: `g++ main.cpp -o main`
+- After running the command, an executable called `main.exe` will be created in the repo. To run the encryption and decryption for the Vigenere Cipher, run the following commands respectively: `./main vigenere encrypt example.txt` and `./main vigenere decrypt encrypted-file.txt`. Make sure you run the encryption command first before running the decryption command.
+- To run the encryption and decryption for the Columnar Transposition Cipher, run the following commands respectively: `./main transposition encrypt example.txt` and `./main transposition decrypt encrypted-file.txt`. Make sure you run the encryption command first before running the decryption command.
--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -15,7 +17,7 @@ string vigenereEncrypt(const string& plaintext, const string& keyword) {
             ciphertext += cipherChar;
             keyIndex = (keyIndex + 1) % keyword.length();
         } else {
-            ciphertext += plainChar; // Non-alphabetic characters remain unchanged
+            ciphertext += plainChar;
         }
     }
 
@@ -33,7 +35,57 @@ string vigenereDecrypt(const string& ciphertext, const string& keyword) {
             plaintext += plainChar;
             keyIndex = (keyIndex + 1) % keyword.length();
         } else {
-            plaintext += cipherChar; // Preserve non-alphabetic characters unchanged
+            plaintext += cipherChar;
+        }
+    }
+
+    return plaintext;
+}
+
+string cTEncrypt(const string& plaintext, const string& keyword) {
+    int keywordLength = keyword.length();
+    vector<string> columns(keywordLength);
+
+    for (int i = 0; i < plaintext.length(); ++i) {
+        columns[i % keywordLength] += plaintext[i];
+    }
+
+    string sortedKeyword = keyword;
+    sort(sortedKeyword.begin(), sortedKeyword.end());
+    string ciphertext;
+    for (char c : sortedKeyword) {
+        int index = keyword.find(c);
+        ciphertext += columns[index];
+    }
+
+    return ciphertext;
+}
+
+string cTDecrypt(const string& ciphertext, const string& keyword) {
+    int keywordLength = keyword.length();
+    int columnLength = ciphertext.length() / keywordLength;
+    vector<string> columns(keywordLength);
+
+    string ciphertextCopy = ciphertext;
+
+    int incompleteColumns = ciphertext.length() % keywordLength;
+
+    int idx = 0;
+    for (char c : keyword) {
+        int count = columnLength;
+        if (idx < incompleteColumns) {
+            count++;
+        }
+        columns[idx++] = ciphertextCopy.substr(0, count);
+        ciphertextCopy.erase(0, count);
+    }
+
+    string plaintext;
+    for (int i = 0; i < columnLength + (incompleteColumns > 0 ? 1 : 0); ++i) {
+        for (const string& column : columns) {
+            if (i < column.length()) {
+                plaintext += column[i];
+            }
         }
     }
 
@@ -56,7 +108,7 @@ int main(int argc, char* argv[]) {
             outputFile << vigenereEncrypt(line, key) << endl;
         }
 
-        std::cout << "File has been encrypted! Please check the encrypted-file.txt file in the program." << endl;
+        cout << "File has been encrypted! Please check the encrypted-file.txt file in the program." << endl;
     }
     // ./main vigenere decrypt encrypted-file.txt
     else if(argc < 5 && type == "vigenere" && action == "decrypt") {
@@ -68,10 +120,34 @@ int main(int argc, char* argv[]) {
             outputFile << vigenereDecrypt(line, key) << endl;
         }
 
-        std::cout << "File has been decrypted! Please check the decrypted-file.txt file in the program." << endl;
+        cout << "File has been decrypted! Please check the decrypted-file.txt file in the program." << endl;
+    }
+    // ./main transposition encrypt example.txt
+    else if(argc < 5 && type == "transposition" && action == "encrypt") {
+        ifstream inputFile(fileName);
+        ofstream outputFile("encrypted-file.txt");
+
+        string line;
+        while(getline(inputFile, line)){
+            outputFile << cTEncrypt(line, key) << endl;
+        }
+
+        cout << "File has been encrypted! Please check the encrypted-file.txt file in the program." << endl;
+    }
+    // ./main transposition decrypt encrypted-file.txt
+    else if(argc < 5 && type == "transposition" && action == "decrypt") {
+        ifstream inputFile("encrypted-file.txt");
+        ofstream outputFile("decrypted-file.txt");
+
+        string line;
+        while(getline(inputFile, line)){
+            outputFile << cTDecrypt(line, key) << endl;
+        }
+
+        cout << "File has been decrypted! Please check the decrypted-file.txt file in the program." << endl;
     }
     else {
-        std::cout << "Invalid command! Please enter a valid one." << endl;
+        cout << "Invalid command! Please enter a valid one." << endl;
     }
 
     return 0;
